60d44e6ff811799a430687d8cad0a116e18ab273
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-08-25 13:12:34 +0000

    Tue, Aug 25, 2026, 4:12 PM +03:00

--- app.c
+++ app.c
@@ -3,13 +3,35 @@
 int main(void)
 {
 
+int x =9;
+int y=5;
+
    if (x<y)
 
       {
 
-            printf("x is biger\n"); 
+            printf("x is less than y\n");
 
 
 	  }
 
+
+	    else if (x>y)
+
+      {
+
+            printf("x is greater than y\n");
+
+
+	  }
+
+
+	       else (x==y)
+
+      {
+
+            printf("x is equal to y\n");
+
+
+	  }
 }
