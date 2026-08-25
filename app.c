39b5da7b8df355ec9af393886c848ca4566162c8
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-08-25 20:23:18 +0300

    Fix calculator operations

--- app.c
+++ app.c
@@ -8,41 +8,37 @@ int main(void)
     // Print greeting
     printf("Hello, %s\n", name);
 
-  int x=get_int(" x ");
-  int y=get_int(" y ");
-   char operaction=get_char("operation (+,-,*,/): ");
-        float result;
-         if(operaction=='+')
+   int x = get_int("x: ");
+   int y = get_int("y: ");
+   char operation = get_char("operation (+, -, *, /): ");
+   float result;
 
+   if (operation == '+')
+   {
+      result = x + y;
+   }
+   else if (operation == '-')
+   {
+      result = x - y;
+   }
+   else if (operation == '*')
+   {
+      result = x * y;
+   }
+   else if (operation == '/')
+   {
+      if (y == 0)
+      {
+         printf("Cannot divide by zero.\n");
+         return 1;
+      }
+      result = (float) x / y;
+   }
+   else
+   {
+      printf("Invalid operation.\n");
+      return 1;
+   }
 
-          {
-
-             result = x + y;
-
-
-          }
-
-          else if(operaction=='-')
-         {
-            result = x - y;
-
-         }
-
-
-
-          else if(operaction=='*')
-         {
-             result = x * y;
-         }
-
-          else if(operaction=='/')
-         {
-             result = x / y;
-         }
-
-          else
-         {
-            printf("Invalid operation");
-         }
-
-        }
+   printf("Result: %.2f\n", result);
+}
