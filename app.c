4490ca841490f3f89210edae4720178dc02f4049
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-08-25 13:55:46 +0000

    Tue, Aug 25, 2026, 4:55 PM +03:00

--- app.c
+++ app.c
@@ -3,18 +3,42 @@
 int main(void)
 {
 
-      string answer = get_string("what is ur name? ")
+     string answer = get_string("what is ur name? ");
 
-	  printf("Hello, %s\n", answer );
 
-	  if(x>y)
-	  {
-		  printf("x is greater than y\n");
-	  }
-	  else
-	  {
-		  printf("y is greater than x\n");
-	  }
+    	 printf("Hello, %s\n", answer );
 
+
+		 int x=get_int("enter a number: ");
+         int y=4;
+
+       if (x<y)
+
+	   {
+
+           prrintf("x is less than y\n");
+
+
+
+	   }
+
+
+
+
+
+	    else if (x>y)
+		{
+
+
+
+
+			printf("x is greater than y\n");
+
+		}
+
+		else
+		{
+			printf("x is equal to y\n");
+		}
 
 }
