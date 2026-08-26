243479ca9ef7fa7f8e6edfab7bcce91bde165b7a
Omar925-afk <140343627+Omar925-afk@users.noreply.github.com>
2026-08-26 10:09:08 +0000

    Wed, Aug 26, 2026, 1:09 PM +03:00

--- app.c
+++ app.c
@@ -4,8 +4,8 @@ int main(void)
 
 {
 
-       int x= 3;
-       int y= 4;
+       int x= get_int("enter a number: ");
+       int y= get_int("enter another number: ");
        int z= x+y;
        printf("the sum of %i and %i is %i\n", x, y, z);
 
